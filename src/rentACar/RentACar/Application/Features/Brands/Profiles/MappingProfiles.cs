043b6a2fdb3fcdfb5b3d2cc10d53beea7e84d2d4
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-03 12:30:37 +0300

    query implementasyonu ve sayfalama

--- src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
 using Domain.Entities;
 using Microsoft.Win32.SafeHandles;
 using System;
@@ -16,6 +19,8 @@ namespace Application.Features.Brands.Profiles
         {
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
             CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+            CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+            CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
 
         }
     }

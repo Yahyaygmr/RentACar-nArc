3608e15f271cbef54a1a1d9e90256f842fadf09a
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-09 17:23:26 +0300

    dynamic query altyapısının implementasyonu

--- src/rentACar/RentACar/WebApi/Controllers/ModelsController.cs
+++ src/rentACar/RentACar/WebApi/Controllers/ModelsController.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Features.Brands.Queries.GetList;
 using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,13 @@ namespace WebApi.Controllers
         {
             GetListModelQuery query = new() { PageRequest = pageRequest };
             GetListResponse<GetListModelListItemDto> response = await Mediator.Send(query);
+            return Ok(response);
+        }
+        [HttpPost("GetList/GetListByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery = null)
+        {
+            GetListByDynamicModelQuery query = new() { PageRequest = pageRequest, DynamicQuery = dynamicQuery };
+            GetListResponse<GetListByDynamicModelListItemDto> response = await Mediator.Send(query);
             return Ok(response);
         }
     }

45525847613f410401f12995eb670eb791fb00e3
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-09 15:57:01 +0300

    entity configuration ve repository implementasyonu

--- src/rentACar/RentACar/Application/Services/Repositories/IBrandRepository.cs
+++ src/rentACar/RentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -6,9 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Services.Repositories
+namespace Application.Services.Repositories;
+
+public interface IBrandRepository : IAsyncRepository<Brand, Guid>, IRepository<Brand, Guid>
 {
-    public interface IBrandRepository : IAsyncRepository<Brand, Guid>, IRepository<Brand, Guid>
-    {
-    }
 }
---
+++ src/rentACar/RentACar/Persistence/EntityConfigurations/CarConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations;
+
+public class CarConfiguration : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.ToTable("Cars").HasKey(b => b.Id);
+
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
+        builder.Property(b => b.ModelId).HasColumnName("ModelId").IsRequired();
+        builder.Property(b => b.Kilometer).HasColumnName("Kilometer").IsRequired();
+        builder.Property(b => b.CarState).HasColumnName("CarState").IsRequired();
+        builder.Property(b => b.ModelYear).HasColumnName("ModelYear").IsRequired();
+
+        builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+        builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
+
+        builder.HasIndex(indexExpression: b => b.Name, name: "UK_Cars_Name").IsUnique();
+
+        builder.HasOne(b => b.Model);
+
+        builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
+    }
+}

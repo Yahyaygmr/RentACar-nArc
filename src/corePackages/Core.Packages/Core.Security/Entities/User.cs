a2b41e3699e623700f6524d13c428e479536a9be
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-20 17:02:28 +0300

    securith helpers and extensions

--- src/corePackages/Core.Packages/Core.Security/Entities/User.cs
+++ src/corePackages/Core.Packages/Core.Security/Entities/User.cs
@@ -9,7 +9,6 @@ namespace Core.Security.Entities
 {
     public class User : Entity<int>
     {
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }

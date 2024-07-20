21a2a2a428676da411888ba2531031365491eb29
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-20 19:02:34 +0300

    create user command implementasyonu

--- src/rentACar/RentACar/Application/Features/Users/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar/Application/Features/Users/Profiles/MappingProfiles.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Features.Users.Commands.Create;
+using AutoMapper;
+using Core.Security.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Users.Profiles
 {
-    internal class MappingProfiles
+    public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<User, CreateUserCommand>().ReverseMap();
+            CreateMap<User, CreatedUserResponse>().ReverseMap();
+        }
     }
 }
